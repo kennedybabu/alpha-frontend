0601d550becc0c789d16722f1c8eddeed11723cf
kennedybabu <kennedynjogu8@gmail.com>
2022-09-26 22:30:33 +0300

    fix: uninstall latest version of react-router-dom, i the version 5.3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./components/Header";
 import PostsListPage from "./pages/PostsListPage";
+import { BrowserRouter as Router, Route} from "react-router-dom"
+
 
 function App() {
   return (
-    <div>
-      <Header />
-      <PostsListPage />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={PostsListPage}/>
+      </div>
+    </Router>
   );
 }
 
